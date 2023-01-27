b7b6329b68ce128e8d90fc926419b7a974dc35a4
anu0711 <109150601+anu0711@users.noreply.github.com>
2023-01-27 12:10:22 +0530

    entity model

---
+++ Garage Management/Garage Management.DAL/Model/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_Management.DAL.Model
+{
+    public class Entity
+    {
+        public DateTime DateCreated { get; set; }
+
+        public DateTime DateModified { get; set; }
+
+        public Guid CreatedBy { get; set; }
+
+        public Guid UpdatedBy { get; set; }
+    }
+}
