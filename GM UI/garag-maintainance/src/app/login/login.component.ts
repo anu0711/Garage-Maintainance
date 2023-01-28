642db14ac977217c3b319acd4ee1877677bf0cc6
Nirshee <109128000+Nirshee@users.noreply.github.com>
2023-01-28 13:59:26 +0530

    Add redirect and error handeling

--- GM UI/garag-maintainance/src/app/login/login.component.ts
+++ GM UI/garag-maintainance/src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -10,7 +12,7 @@ export class LoginComponent {
   loginForm: FormGroup;
   loginCount: number = 0;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private router: Router) {
     this.loginForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required]
@@ -19,11 +21,14 @@ export class LoginComponent {
 
   onSubmit() {
     console.log(this.loginForm.value);
-    if (this.loginForm.valid) {
+    if (localStorage) {
       const loginInfo = [this.loginForm.value.email, this.loginForm.value.password];
       localStorage.setItem(`loginInfo${this.loginCount}`, JSON.stringify(loginInfo));
       this.loginCount++;
       console.log('Successful login');
+      timer(5000).subscribe(() => this.router.navigate(['/register']));
+    } else {
+      console.log('localStorage is not available');
     }
   }
 }
