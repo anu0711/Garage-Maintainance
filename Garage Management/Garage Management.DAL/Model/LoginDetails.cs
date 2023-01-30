e3e6932f128bb92100fe91bf6cbaa64b65a58374
Sakthivelan08 <velansakthi52@gmail.com>
2023-01-30 14:22:11 +0530

    Login Model

---
+++ Garage Management/Garage Management.DAL/Model/LoginDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_Management.DAL.Model
+{
+    public class LoginDetails : Entity
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string EmailId { get;set; }  
+        public Guid EmployeeId { get; set; }
+
+    }
+}
